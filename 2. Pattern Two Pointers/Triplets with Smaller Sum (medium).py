4d914c1ba990867728137418818fef8405ad7c21
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2022-10-15 09:33:42 +0530

    Solution

---
+++ 2. Pattern Two Pointers/Triplets with Smaller Sum (medium).py
@@ -0,0 +1,31 @@
+'''
+Problem Statement 
+Given an array arr of unsorted numbers and a target sum, count all triplets in it such that arr[i] + arr[j] + arr[k] < target where i, j, and k are three different indices. Write a function to return the count of such triplets.
+Example 1:
+Input: [-1, 0, 2, 3], target=3 
+Output: 2
+Explanation: There are two triplets whose sum is less than the target: [-1, 0, 3], [-1, 0, 2]
+Example 2:
+Input: [-1, 4, 2, 1, 3], target=5 
+Output: 4
+Explanation: There are four triplets whose sum is less than the target: 
+   [-1, 1, 4], [-1, 1, 3], [-1, 1, 2], [-1, 2, 3]
+'''
+
+#mycode
+def triplet_with_smaller_sum(arr, target):
+  count=0
+  for i in range(len(arr)-3):
+      for j in range(i+1,len(arr)):
+          for k in range(j+1,len(arr)):
+              if arr[i]+arr[j]+arr[k]<t:
+                  count+=1
+  return count
+
+
+def main():
+  print(triplet_with_smaller_sum([-1, 0, 2, 3], 3))
+  print(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5))
+
+
+main()
