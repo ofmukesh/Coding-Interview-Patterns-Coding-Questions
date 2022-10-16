c083d7b3f50b798fddbb8f7d4a347f62816dbf0c
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2022-10-16 11:39:35 +0530

    solution

---
+++ 1. Pattern Sliding Window/Longest Subarray with Ones after Replacement (hard).py
@@ -0,0 +1,40 @@
+'''
+Problem Statement 
+Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s, find the length of the longest contiguous subarray having all 1s.
+Example 1:
+Input: Array=[0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2
+Output: 6
+Explanation: Replace the '0' at index 5 and 8 to have the longest contiguous subarray of 1s having length 6.
+Example 2:
+Input: Array=[0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], k=3
+Output: 9
+Explanation: Replace the '0' at index 6, 9, and 10 to have the longest contiguous subarray of 1s having length 9.
+'''
+
+#mycode
+def length_of_longest_substring(arr, k):
+  Result=0
+
+  for i in range(len(Array)):
+      rep=0
+      count=0
+      for j in range(i,len(Array)):
+          if Array[j]==1:
+              count+=1;
+          elif rep<k:
+              count+=1;
+              rep+=1
+          else:
+              break
+      Result=max(Result,count)
+  return Result
+
+
+def main():
+  print(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2))
+  print(length_of_longest_substring(
+    [0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3))
+
+
+main()
+
